c8fcb7d2296f79472707afb03cfe1d30ada2a186
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-05 08:31:36 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ require('dotenv').config()
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB,
