b784511a97fd64454008f68a8abf4aea7d8c2520
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-05 09:51:58 +0100

    changes

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -4,13 +4,16 @@ import { Response } from '../interfaces/response.interface';
 import { CharacterService } from '../services/character.service';
 import { CreateCharacter } from '../dtos/character.dto';
 import { GetCharactersQueryDto } from '../dtos/getCharacterQuery.dto';
-
+import { ApiBody, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger'
 @Controller('character')
 export class CharacterController {
     constructor(
         private readonly characterService: CharacterService,
       ) { }
+
     @Post()
+    @ApiCreatedResponse({description:'Character Creation Response'})
+    @ApiBody({type:CreateCharacter})
     async createCharacter(@Body() payload:CreateCharacter):Promise<Response>{
         const character = await this.characterService.addCharacter(payload)
         return Helpers.sendJsonResponse( character ,'character has beeen created')
@@ -18,6 +21,7 @@ export class CharacterController {
 
 
     @Get()
+    @ApiOkResponse({description:'Get all characters response'})
     async getAllCharacters(@Query() queryParms:GetCharactersQueryDto):Promise<Response>{
         const characters = await this.characterService.getAllCharacters(queryParms)
         return Helpers.sendJsonResponse( characters ,'All Characters')
--- src/dtos/character.dto.ts
+++ src/dtos/character.dto.ts
@@ -1,6 +1,7 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { Episode } from "../interfaces/episode.interface";
 import { Location } from "../interfaces/location.interface";
+import { ApiProperty } from "@nestjs/swagger"
 
 export enum status{
     ACTIVE="ACTIVE",
@@ -18,23 +19,28 @@ export class CreateCharacter{
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({type:String, description:'firstname'})
     firstname:string
 
     
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({type:String, description:'lastname'})
     lastname:string
 
     @IsNotEmpty()
     @IsEnum(status)
+    @ApiProperty({type:String, description:'status'})
     status:string
 
     @IsString()
     @IsOptional()
+    @ApiProperty({type:String, description:'stateOfOrigin'})
     stateOfOrigin:string
 
     @IsNotEmpty()
     @IsEnum(gender)
+    @ApiProperty({type:String, description:'gender'})
     gender:string
 
     @IsOptional()
